1005bf411b8f73e9c45f116340353394c628a8fd
THIBAUD Ludovic <ludovic.thibaud@maif.fr>
2026-07-01 13:29:39 +0200

    add value to enum

--- backend/src/main/java/fr/simplon/backend/model/enums/ResourceStatus.java
+++ backend/src/main/java/fr/simplon/backend/model/enums/ResourceStatus.java
@@ -1,7 +1,14 @@
 package fr.simplon.backend.model.enums;
 
 public enum ResourceStatus {
-    TODO,
-    IN_PROGRESS,
-    DONE;
+    TODO("A faire"),
+    IN_PROGRESS("En cours"),
+    DONE("Terminé");
+
+    private final String label;
+
+    private ResourceStatus(String label) {
+        this.label = label;
+    }
 }
+
