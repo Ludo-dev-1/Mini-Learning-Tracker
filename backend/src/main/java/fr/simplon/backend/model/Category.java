c41087bbea478d94ca8b38e5ac7eab1e8ff141c9
GOMES Mathéo <matheo.gomes@externe.maif.fr>
2026-07-01 13:16:35 +0200

    Category entityaaaa

---
+++ backend/src/main/java/fr/simplon/backend/model/Category.java
@@ -0,0 +1,32 @@
+package fr.simplon.backend.model;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+    
+    @OneToMany(mappedBy = "category")
+    private List<Ressource> ressources;
+}
