fa1dc9f47b90a6eda88535ef053891fe18d69ff2
THIBAUD Ludovic <ludovic.thibaud@maif.fr>
2026-07-01 15:11:15 +0200

    add RessourceService

--- backend/src/main/java/fr/simplon/backend/repository/ResourceRepository.java
+++ backend/src/main/java/fr/simplon/backend/repository/ResourceRepository.java
@@ -1,13 +1,18 @@
 package fr.simplon.backend.repository;
 
 import fr.simplon.backend.model.Resource;
+import fr.simplon.backend.model.enums.ResourceStatus;
+import fr.simplon.backend.model.enums.ResourceType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
-public interface ResourceRepository extends JpaRepository<Resource, Integer> {
+public interface ResourceRepository extends JpaRepository<Resource, Long> {
 
-    List<Resource> findResourceById(Long id);
+    Optional<Resource> findByType(ResourceType type);
+
+    Optional<Resource> findByStatus(ResourceStatus status);
 }
---
+++ backend/src/main/java/fr/simplon/backend/service/ResourceService.java
@@ -0,0 +1,31 @@
+package fr.simplon.backend.service;
+
+import fr.simplon.backend.model.Resource;
+import fr.simplon.backend.model.enums.ResourceStatus;
+import fr.simplon.backend.model.enums.ResourceType;
+import fr.simplon.backend.repository.ResourceRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ResourceService {
+
+    private final ResourceRepository resourceRepositoryinjected;
+
+    public ResourceService(ResourceRepository resourceRepositoryinjected) {
+        this.resourceRepositoryinjected = resourceRepositoryinjected;
+    }
+
+    public Optional<Resource> findById(Long resourceId) {
+        return this.resourceRepositoryinjected.findById(resourceId);
+    }
+
+    public Optional<Resource> findByType(ResourceType resourceType) {
+        return this.resourceRepositoryinjected.findByType(resourceType);
+    }
+
+    public Optional<Resource> findByStatus(ResourceStatus resourceStatus) {
+        return this.resourceRepositoryinjected.findByStatus(resourceStatus);
+    }
+}
