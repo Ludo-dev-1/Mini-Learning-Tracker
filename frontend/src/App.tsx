f19fdb2d11a22f94acebefdd35e23bef858959e1
THIBAUD Ludovic <ludovic.thibaud@maif.fr>
2026-07-02 16:15:06 +0200

    début d'implémentation du filtre par type

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,34 @@
-
 import './App.css'
 import {CardCategory} from "./component/CardCategory.tsx";
+import {useState} from "react";
 
 function App() {
 
+  // state pour stocker les articles
+  const [resources, setResource] = useState([]);
+
+  // state pour stocker le type sélectionné
+  const [selectedType, setSelectedType] = useState("All");
+
+  const filteredResourceByType =
+      selectedType === "All"
+          ? resources
+          : resources.filter((resources) => resource.type.some((typ) => typ.name === selectedType));
 
 
   return (
     <>
+      {/* Sélecteur de type */}
+      <div>
+        <select onChange={(e) => setSelectedType(e.target.value)}>
+          <option value="All">Type</option>
+
+        </select>
+
+      </div>
+
+
+
     <CardCategory />
     </>
 
