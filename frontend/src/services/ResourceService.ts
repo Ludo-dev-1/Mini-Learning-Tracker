0304f9088d460d49a240fb32e14f68124f679021
GOMES Mathéo <matheo.gomes@externe.maif.fr>
2026-07-02 14:02:51 +0200

    Reolved conflicts + Resources Service

---
+++ frontend/src/services/ResourceService.ts
@@ -0,0 +1,36 @@
+// GET    /api/resources
+// GET    /api/resources/{resourceId}
+// POST   /api/resources
+
+let server_url: string = "http://localhost:8080/api/";
+let endpoint: string = "resources";
+
+export async function getResources() {
+  return await fetch(server_url + endpoint)
+    .then((resp) => resp.json())
+    .catch((err) => err.message);
+}
+
+export async function getRessourceById(id: number) {
+  return await fetch(server_url + endpoint + "/" + id)
+    .then((resp) => resp.json())
+    .catch((err) => err.message);
+}
+
+type Resource = {
+  title: string;
+  description: string;
+  url: string;
+  type: string;
+  status: string;
+  created_at: string;
+  category: number;
+};
+
+export async function postResource(r: Resource) {
+  return await fetch(server_url, {
+    method: "POST",
+    body: JSON.stringify({ r }),
+  }).catch(err => err.message);
+}
+console.log("check");
