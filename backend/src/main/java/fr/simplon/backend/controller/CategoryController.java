d9d807a2a3ccc15e9ee8105039cdcfedfd0ebe66
Neuroliminalys-02 <147269298+neuroliminalys@users.noreply.github.com>
2026-07-02 14:04:22 +0200

    Reolved conflicts + Resources Service (#35)
    Co-authored-by: GOMES Mathéo <matheo.gomes@externe.maif.fr>

--- backend/src/main/java/fr/simplon/backend/controller/CategoryController.java
+++ backend/src/main/java/fr/simplon/backend/controller/CategoryController.java
@@ -3,9 +3,7 @@ package fr.simplon.backend.controller;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.simplon.backend.model.Category;
-import fr.simplon.backend.service.CategoryService;
 import fr.simplon.backend.usecases.CategoryUsecase;
-import fr.simplon.backend.usecases.categoryUsecase;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,9 +13,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
-
-
 
 @RestController
 @RequestMapping("/api/categories")
