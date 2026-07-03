fc9da411bdf6bcfcd8c4c8f808a5b0928f551954
GOMES Mathéo <matheo.gomes@externe.maif.fr>
2026-07-03 09:21:58 +0200

    Working resourceList component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import {CardCategory} from "./component/CardCategory.tsx";
+import { ResourceList } from './component/resourceList/ResourceList.tsx';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
     <CardCategory />
+    <ResourceList />
     </>
 
   )
